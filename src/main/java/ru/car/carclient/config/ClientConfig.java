6b4b8a586a1404bbdcc016419d8b4a8d14295eb8
YakovMalykh <to-yakoff@mail.ru>
2023-03-18 15:41:57 +0300

    initial commit

--- src/main/java/ru/car/carclient/config/ClientConfig.java
+++ src/main/java/ru/car/carclient/config/ClientConfig.java
@@ -1,2 +1,15 @@
-package ru.car.carclient.config;public class ClientConfig {
+package ru.car.carclient.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.RSocketRequester;
+
+@Slf4j
+@Configuration
+public class ClientConfig {
+    @Bean
+    RSocketRequester requester(RSocketRequester.Builder requestBuilder) {
+        return requestBuilder.tcp("localhost", 7000);
+    }
 }
