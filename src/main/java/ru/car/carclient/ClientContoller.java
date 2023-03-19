0adf5e140a5877266d78a4a1a1ff389754c5dd82
YakovMalykh <to-yakoff@mail.ru>
2023-03-19 16:58:00 +0300

    improvements

--- src/main/java/ru/car/carclient/ClientContoller.java
+++ src/main/java/ru/car/carclient/ClientContoller.java
@@ -1,5 +1,6 @@
 package ru.car.carclient;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +17,17 @@ public class ClientContoller {
     // http://localhost:8080/webjars/swagger-ui/index.html#/
     @Autowired
     RequestService requestService;
-
+    @Operation(summary = "прием данных синхронизации от пользователя")
     @PostMapping("{uuid}")
     public Mono<ResponseEntity<Void>> synchronizeInfo(@PathVariable UUID uuid, @RequestParam String json) {
         return requestService.saveUserInfo(uuid, json);
     }
-
+    @Operation(summary = "отправка данных пользователю")
     @GetMapping("/{uuid}")
     public Mono<InfoFromDbDto> getInfoByUuid(@PathVariable UUID uuid) {
         return requestService.getInfoByUuid(uuid).log();
     }
-
+    @Operation(summary = "прием игровой статистики от пользователя ")
     @PostMapping("/activity/{uuid}")
     public Mono<ResponseEntity<Void>> saveActivity(@PathVariable UUID uuid, @RequestParam Long activity) {
         return requestService.saveActivity(uuid, activity);
