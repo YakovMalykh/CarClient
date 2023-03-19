4d790a7944f5cea8f1a0abc706e1e95c9909e5eb
YakovMalykh <to-yakoff@mail.ru>
2023-03-19 23:43:20 +0300

    прописал dockerfile

--- src/main/java/ru/car/carclient/config/ClientConfig.java
+++ src/main/java/ru/car/carclient/config/ClientConfig.java
@@ -1,6 +1,7 @@
 package ru.car.carclient.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.rsocket.RSocketRequester;
@@ -8,8 +9,10 @@ import org.springframework.messaging.rsocket.RSocketRequester;
 @Slf4j
 @Configuration
 public class ClientConfig {
+    @Value("${path.to.server}")
+    private String pathToServer;
     @Bean
     RSocketRequester requester(RSocketRequester.Builder requestBuilder) {
-        return requestBuilder.tcp("localhost", 7000);
+        return requestBuilder.tcp(pathToServer, 7000);
     }
 }
